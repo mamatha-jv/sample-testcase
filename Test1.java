71027af0c6036aa5579e244f0bfdc06768abc951
Mamatha J V <mamatha.jv@in.ibm.com>
2019-10-18 04:51:40 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,10 @@
+package sample-testcase;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.print("Example Test Run, Java version: ");
+		System.out.println(System.getProperty("java.version"));
+	}
+
+}
